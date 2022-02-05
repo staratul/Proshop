56efeafae6ef331009006be790be83b3fb07d402
Atul Chauhan <atulji.star@gmail.com>
2022-02-05 21:58:28 +0530

    add to cart changes chcking

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Product from '../components/Product'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import { Col, Row } from 'react-bootstrap'
 import { listProducts } from '../actions/productActions'
 
@@ -18,7 +20,7 @@ const HomeScreen = () => {
         <div>
             <h1>Latest Products</h1>
             {
-                loading ? <h2>Loading...</h2> : error ? <h3>{error}</h3> : 
+                loading ? <Loader /> : error ? <Message variant="danger">{error}</Message> : 
                 <Row>
                     {products.map(product => (
                         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
