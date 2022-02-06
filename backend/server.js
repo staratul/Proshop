0cfe06d149a8547585b39f971b4e2e82f994f319
Atul Chauhan <atulji.star@gmail.com>
2022-02-06 11:26:38 +0530

    user authontication and authorization

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from 'colors'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoute.js'
+import userRoutes from './routes/userRoute.js'
 
 
 dotenv.config()
@@ -12,11 +13,15 @@ await connectDB();
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('API is running....')
 })
 
 app.use('/api/products', productRoutes)
+
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
